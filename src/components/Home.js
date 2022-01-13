dc2e75604cc1e2ca087a70386acfb7f6318c31c2
KayLemba <kaylemba@yahoo.com>
2022-01-13 16:09:29 +0200

    Edit Home.js file

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Home = () => (
   <div className="container mb-5">
-    <h2 className="h1 my-5">Welcome TO our Page!</h2>
+    <h2 className="h1 my-5">Welcome To Our Page!</h2>
     <p className="h5 my-5">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      In vero dolores tenetur dignissimos? Laborum fuga sit
-      maxime eum veritatis dolor ipsam dolorem dolores amet,
-      officia distinctio minima consequuntur aliquam ratione,
-      sapiente explicabo architecto.
+      Math Magicians is a single page application (SPA)
+      built with React for people who love mathematics.
+      It helps to make calculations straight forward and easy.
+      Mathematics provides an effective way of building mental discipline and
+      encourages logical reasoning and mental rigor.
       Autem alias laudantium natus totam! Adipisci, autem. Eius
       distinctio modi mollitia incidunt eum accusamus
       fugiat dolor eligendi illum, architecto autem assumenda, sequi
